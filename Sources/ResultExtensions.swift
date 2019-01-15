60c57615dae8ac01ae7105a415d34a20f1a7caf7
NachoSoto <ignaciosoto90@gmail.com>
2019-01-14 16:32:14 -0800

    Added NoError and ResultProtocol for Swift.Result

--- Sources/ResultExtensions.swift
+++ Sources/ResultExtensions.swift
@@ -3,3 +3,39 @@ extension Result: SignalProducerConvertible {
 		return .init(result: self)
 	}
 }
+
+/// An “error” that is impossible to construct.
+///
+/// This can be used to describe `Result`s where failures will never
+/// be generated. For example, `Result<Int, NoError>` describes a result that
+/// contains an `Int`eger and is guaranteed never to be a `failure`.
+public enum NoError: Swift.Error, Equatable {
+	public static func ==(lhs: NoError, rhs: NoError) -> Bool {
+		return true
+	}
+}
+
+/// A protocol that can be used to constrain associated types as `Result`.
+public protocol ResultProtocol {
+	associatedtype Success
+	associatedtype Failure: Swift.Error
+	
+	init(success: Success)
+	init(failure: Failure)
+	
+	var result: Result<Success, Failure> { get }
+}
+
+extension Result: ResultProtocol {
+	public init(success: Success) {
+		self = .success(success)
+	}
+	
+	public init(failure: Failure) {
+		self = .failure(failure)
+	}
+	
+	public var result: Result<Success, Failure> {
+		return self
+	}
+}
